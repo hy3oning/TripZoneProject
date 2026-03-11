6a238e900537d93fb53cd448751165aebbc1aeca
marronnier31 <marronnier31@gmail.com>
2026-03-11 20:03:07 +0900

    [추가] 다양한 domain에서  value값과 label값을 codeLabelValueDTO에 간단하게 저장하기 위한 메서드 추가 & 실제 서비스단에서 사용하는 방법에 대한 예시를 주석으로 표시

--- src/main/java/com/kh/trip/dto/CodeLabelValueDTO.java
+++ src/main/java/com/kh/trip/dto/CodeLabelValueDTO.java
@@ -14,4 +14,20 @@ import lombok.NoArgsConstructor;
 public class CodeLabelValueDTO {
 	private String value; // group_code (예: "A01")
 	private String label; // group_name (예: "관리자")
+
+	// 범용 변환 메서드 (정적 팩토리 메서드)
+	// Extractor는 전체 데이터 객체에서 내가 필요한 특정 데이터만 뽑아내기 위해 정의한 함수(추출기)
+	public static <T> CodeLabelValueDTO of(T entity, Function<T, String> valueExtractor,
+			Function<T, String> labelExtractor) {
+		return CodeLabelValueDTO.builder().value(valueExtractor.apply(entity)).label(labelExtractor.apply(entity)).build();
+	}
+	
+	/*** 실제 서비스 단에서 사용하는 예시 ***
+	public List<CodeLabelValue> getGroupCodes() {
+    List<GroupCode> list = repository.findAll();
+    return list.stream()
+               .map(entity -> CodeLabelValue.of(entity, GroupCode::getGroupCode, GroupCode::getGroupName))
+               .toList();
+	}
+	*/
 }
