ce97200d91e335a858f3215e426bfce99e180e53
marronnier31 <marronnier31@gmail.com>
2026-03-16 14:34:18 +0900

    [update] InquiryMessage Entity 완성

--- src/main/java/com/kh/trip/domain/InquiryMessage.java
+++ src/main/java/com/kh/trip/domain/InquiryMessage.java
@@ -1,5 +1,7 @@
 package com.kh.trip.domain;
 
+import com.kh.trip.domain.common.BaseTimeEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -24,7 +26,7 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class InquiryMessage {
+public class InquiryMessage extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_INQUIRY_MESSAGES")
 	@SequenceGenerator(name = "SEQ_INQUIRY_MESSAGES", sequenceName = "SEQ_INQUIRY_MESSAGES", allocationSize = 1)
@@ -35,7 +37,19 @@ public class InquiryMessage {
 	@JoinColumn(name = "INQUIRY_ROOM_NO", nullable = false)
 	private InquiryRoom inquiryRoom;
 	
+	@Column(name = "SENDER_USER_NO", nullable = false)
+	private Long senderUserNo;
 	
+	@Column(name = "SENDER_TYPE", nullable = false, length = 20)
+	private String senderType;
 	
+	@Column(name = "MESSAGE_CONTENT", length = 2000)
+	private String messageContent;
 	
+	@Column(name = "IMAGE_URL", length = 300)
+	private String imageUrl;
+	
+	@Builder.Default
+	@Column(name = "READ_CHECK", nullable = false)
+	private boolean readcheck = false;//읽으면 TRUE, 안읽으면 false
 }
