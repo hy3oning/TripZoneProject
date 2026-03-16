060dd45951d7e5bdaf960a0bf9b43cda52f6822c
choesoyeong87-ui <choesoyeong87@gmail.com>
2026-03-16 17:50:22 +0900

    [생성]service,serviceImpl/ EventController,EventDTO

---
+++ src/main/java/com/kh/trip/service/EventServiceImpl.java
@@ -0,0 +1,15 @@
+package com.kh.trip.service;
+
+import com.kh.trip.dto.EventDTO;
+import com.kh.trip.dto.PageRequestDTO;
+import com.kh.trip.dto.PageResponseDTO;
+
+public class EventServiceImpl implements EventService{
+
+	@Override
+	public PageResponseDTO<EventDTO> list(PageRequestDTO pageRequestDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
