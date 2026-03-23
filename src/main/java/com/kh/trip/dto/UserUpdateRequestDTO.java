688c8208f840fefb7fce692fc3ce42e65abdae3d
mudi <freedom2424@nate.com>
2026-03-23 18:55:34 +0900

    [생성] user 조회, 삭제, 수정 기능 구현

--- src/main/java/com/kh/trip/dto/UserUpdateRequestDTO.java
+++ src/main/java/com/kh/trip/dto/UserUpdateRequestDTO.java
@@ -1,12 +1,11 @@
 package com.kh.trip.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserUpdateRequestDTO {
-	@NotBlank
-	private String name;
+	
+	private String phone;
 }
