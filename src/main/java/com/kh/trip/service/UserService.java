32d29839533b95fc7b2e1b98c82b523e3e0f9b60
mudi <freedom2424@nate.com>
2026-03-26 18:59:41 +0900

    [update] UserService에 비밀번호 변경 계약 추가

--- src/main/java/com/kh/trip/service/UserService.java
+++ src/main/java/com/kh/trip/service/UserService.java
@@ -1,6 +1,7 @@
 package com.kh.trip.service;
 
 import com.kh.trip.dto.AdminUserSearchRequestDTO;
+import com.kh.trip.dto.PasswordChangeRequestDTO;
 import com.kh.trip.dto.PageResponseDTO;
 import com.kh.trip.dto.UserDTO;
 import com.kh.trip.dto.UserUpdateRequestDTO;
@@ -9,6 +10,7 @@ public interface UserService {
 	
 	UserDTO getUser(Long userNo);
 	void update(Long userNo, UserUpdateRequestDTO request);
+	void changePassword(Long userNo, PasswordChangeRequestDTO request);
 	void delete(Long UserNo);
 	void restore(Long userNo);
 	PageResponseDTO<UserDTO> findUsers(AdminUserSearchRequestDTO request);
