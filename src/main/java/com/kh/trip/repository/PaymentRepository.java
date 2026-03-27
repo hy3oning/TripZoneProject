0c70cb3431c34a93d01f5dc94a52bdb3d6d39ef0
hy3oning <shyun5635@gmail.com>
2026-03-27 15:00:49 +0900

    [update] payment Page기법, preAuthorize 인가 설정

--- src/main/java/com/kh/trip/repository/PaymentRepository.java
+++ src/main/java/com/kh/trip/repository/PaymentRepository.java
@@ -3,17 +3,18 @@ package com.kh.trip.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kh.trip.domain.Payment;
 import com.kh.trip.domain.enums.PaymentStatus;
 
-public interface PaymentRepository extends JpaRepository<Payment, Long>{
-	Optional<Payment> findByPaymentId(String paymentId);
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-	Optional<Payment> findByBooking_BookingNo(Long bookingNo);
+	Optional<Payment> findFirstByBooking_BookingNoOrderByPaymentNoDesc(Long bookingNo);
 
-	List<Payment> findByBooking_BookingNoOrderByPaymentNoDesc(Long bookingNo);
+	Page<Payment> findByBooking_BookingNoOrderByPaymentNoDesc(Long bookingNo, Pageable pageable);
 
 	boolean existsByBooking_BookingNoAndPaymentStatusIn(Long bookingNo, List<PaymentStatus> statuses);
 }
